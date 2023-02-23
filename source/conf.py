4320abeb9a267ffe655a09edbd63335de839afa6
kirs-kirill <kirs-kirill@yandex.ru>
2023-02-23 16:14:50 +0500

    Add files

---
+++ source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'smart-doc'
+copyright = '2023, usem'
+author = 'usem'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx_rtd_dark_mode"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+html_show_sphinx = False
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+epub_language = 'ru'
+epub_title = 'smart-doc'
+epub_author = 'usem'
+epub_publisher = 'usem'
+epub_copyright = '2023, usem'
