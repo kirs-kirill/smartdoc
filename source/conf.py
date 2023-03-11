9e351c6e87ee4223a130f0adf424ef2639fa3a6b
kirs-kirill <79133212+kirs-kirill@users.noreply.github.com>
2023-03-11 09:12:39 +0300

    edit theme

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,11 @@ author = 'usem'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_rtd_dark_mode"]
+extensions = [
+    "sphinx.ext.imgconverter",
+    "sphinx_inline_tabs",
+    "sphinx_copybutton",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,8 +30,23 @@ html_show_sphinx = False
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = ('furo')
 html_static_path = ['_static']
+html_title = "Документация SmartEngin"
+html_theme_options = {
+    "prev_next_buttons_location": "both",
+    "navigation_with_keys": True,
+    "top_of_page_button": None,
+    # "announcement": "",
+    "light_css_variables": {
+        "color-brand-primary": "#336790",  # "blue"
+        "color-brand-content": "#336790",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#E5B62F",  # "yellow"
+        "color-brand-content": "#E5B62F",
+    },
+}
 
 epub_language = 'ru'
 epub_title = 'smart-doc'
