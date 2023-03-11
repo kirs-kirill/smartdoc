a13bae78243f34e84797a8e1ba1934ac118223c3
kirs-kirill <79133212+kirs-kirill@users.noreply.github.com>
2023-03-11 14:04:25 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -33,20 +33,20 @@ html_show_sphinx = False
 html_theme = ('furo')
 html_static_path = ['_static']
 html_title = "Документация SmartEngin"
-html_theme_options = {
-    "prev_next_buttons_location": "both",
-    "navigation_with_keys": True,
-    "top_of_page_button": None,
-    # "announcement": "",
-    "light_css_variables": {
-        "color-brand-primary": "#336790",  # "blue"
-        "color-brand-content": "#336790",
-    },
-    "dark_css_variables": {
-        "color-brand-primary": "#E5B62F",  # "yellow"
-        "color-brand-content": "#E5B62F",
-    },
-}
+#html_theme_options = {
+#    "prev_next_buttons_location": "both",
+#    "navigation_with_keys": True,
+#    "top_of_page_button": None,
+#    # "announcement": "",
+#    "light_css_variables": {
+#        "color-brand-primary": "#336790",  # "blue"
+#        "color-brand-content": "#336790",
+#    },
+#    "dark_css_variables": {
+#        "color-brand-primary": "#E5B62F",  # "yellow"
+#        "color-brand-content": "#E5B62F",
+#    },
+#}
 
 epub_language = 'ru'
 epub_title = 'smart-doc'
